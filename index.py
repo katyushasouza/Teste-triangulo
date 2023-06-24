4b0544853bd4c14c8a579a9ec3b3d236f5d24bd3
Katyusha Souza <47747424+katyushasouza@users.noreply.github.com>
2023-06-24 13:19:00 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,11 @@
-#Desenvolva um programa que indique se as medidas informadas formam um triangulo escaleno (lados diferentes), triangulo isoceles (dois lados iguais) ou 
-#triangulo equilatero (todos os lados iguais) 
+#Desenvolva um programa que indique se as medidas informadas formam um triangulo escaleno (lados diferentes), triangulo isósceles (dois lados iguais) ou 
+#triangulo equilátero (todos os lados iguais) 
 #Considerando que para que as medidas informadas possam formar um triangulo eles devem seguir a regra r1+r2>r3
 print('Analisador de triângulos')
 r1 = float(input('Primeiro lado: '))
 r2 = float(input('Segundo lado: '))
 r3 = float(input('Terceiro lado: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+  print('As medidas informadas formam um triângulo')
+else:
+  print('As medidas informadas não forma um triângulo')
